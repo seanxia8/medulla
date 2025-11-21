c23d4f42feaca6a61a451610fb9f4641144f43e7
seanxia8 <seanxia8@gmail.com>
2025-11-21 14:15:21 -0800

    Added missing selector parameter initialization and fix typo in test toml

--- selection/src/framework.cc
+++ selection/src/framework.cc
@@ -421,11 +421,15 @@ NamedSpillMultiVar construct(const std::vector<cfg::ConfigurationTable> & cuts,
             {
                 // Full name for the variable.
                 std::string full_name = "true_" + var.get_string_field("selector") + "_" + var_name;
-
+                std::vector<double> selectorPars;
+                if (var.has_field("selector_parameters")) {
+                    selectorPars = var.get_double_vector("selector_parameters");
+                }
                 // Retrieve the selector function.
                 std::string selector_name = "true_" + var.get_string_field("selector");
                 auto selector_factory = SelectorFactoryRegistry<TType>::instance().get(selector_name);
-                auto selector = selector_factory(std::vector<double>{});
+                //auto selector = selector_factory(std::vector<double>{});
+                auto selector = selector_factory(selectorPars);
                                 
                 // Retrieve the particle-level variable function.
                 var_name = "true_particle_" + var_name;
@@ -468,11 +472,15 @@ NamedSpillMultiVar construct(const std::vector<cfg::ConfigurationTable> & cuts,
             {
                 // Full name for the variable.
                 std::string full_name = "reco_" + var.get_string_field("selector") + "_" + var_name;
-
+                std::vector<double> selectorPars;
+                if (var.has_field("selector_parameters")) {
+                    selectorPars = var.get_double_vector("selector_parameters");
+                }
                 // Retrieve the selector function.
                 std::string selector_name = "reco_" + var.get_string_field("selector");
                 auto selector_factory = SelectorFactoryRegistry<RType>::instance().get(selector_name);
-                auto selector = selector_factory(std::vector<double>{});
+                //auto selector = selector_factory(std::vector<double>{});
+                selector = selector_factory(selectorPars);
 
                 // Retrieve the particle-level variable function.
                 var_name = "reco_particle_" + var_name;
